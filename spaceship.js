a653818281a8ab6879ca1b2a304637d3242d00e5
Robert Koteles <robert.koteles@freemail.hu>
2021-05-12 22:21:38 +0200

    travel function módosítása

--- spaceship.js
+++ spaceship.js
@@ -4,22 +4,21 @@ const SpaceShip = {
   'passengers': ['John', 'Steve', 'Sam', 'Danielle'],
   'shields': true,
   'speedometer': 0,
-  listPassengers() {
+  'listPassengers': function () {
     for (let i = 0; i < this.passengers.length; i++) {
       console.log('Passenger ' + (i + 1) + ': ' + SpaceShip.passengers[i]);
     }
   },
-  addPassenger(newPassenger) {
+  'addPassenger': function (newPassenger) {
     SpaceShip.passengers.push(newPassenger);
     console.log(newPassenger + ' was added to the ship');
   },
-  travel(distance) {
+  'travel': function (distance) {
     let originalFuel = this.fuel;
     console.log('trying to travel: ' + distance);
     if (this.fuel === 0) {
       console.log('cant go further, tank is empty');
     } else {
-
       this.fuel = this.fuel - (distance / 2);
       if (this.fuel < 0) {
         distance = originalFuel * 2;
@@ -27,14 +26,13 @@ const SpaceShip = {
         this.fuel = 0;
       }
       this.speedometer = this.speedometer + distance;
-      if (this.fuel < 30) {
+      if (this.fuel < 30 && this.shields !== false) {
         this.shields = false;
         console.log('fuel is low, turning off shields... ');
       }
       console.log('the SpaceShip is at: ' + this.speedometer);
       console.log('the SpaceShip has ' + this.fuel + ' fuel');
     }
-
   }
 };
 
